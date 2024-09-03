672b1ee82c7bd382299fb156d1a454cd69557fb6
KRath87 <k.rath@mbd-team.de>
2024-09-03 15:02:56 +0200

    basic design playground

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
 import "./style.css";
+
+const playground = <HTMLDivElement>document.getElementById("playground");
+
+for (let i = 0; i < 225; i++) {
+	const div = document.createElement("div");
+
+	playground.appendChild(div);
+}
