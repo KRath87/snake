75565a6b5e4fd5d6239ff6dd429faa02bab25a1d
KRath87 <k.rath@mbd-team.de>
2024-09-05 12:40:42 +0200

    snake moves, food is random once

--- src/main.ts
+++ src/main.ts
@@ -8,17 +8,33 @@ for (let i = 0; i < 225; i++) {
 	playground.appendChild(div);
 }
 
-const position = {
-	x: 330,
-	y: 330,
+const fieldSize = 700 / 15;
+
+let position = {
+	x: 7 * fieldSize,
+	y: 7 * fieldSize,
 };
 
-let snakeSize = { width: 30, height: 30 };
+let randomX = Math.floor(Math.random() * (15 - 1) + 1);
+let randomY = Math.floor(Math.random() * (15 - 1) + 1);
+console.log(randomX);
+console.log(randomY);
+
+let positionFood = {
+	x: randomX * fieldSize,
+	y: randomY * fieldSize,
+};
+
+let foodCount = 0;
+
+let snakeSize = { width: fieldSize, height: fieldSize };
 
 let snake = <HTMLDivElement>document.getElementById("snake");
 
 const food = <HTMLDivElement>document.getElementById("food");
 
+food.style.top = positionFood.y.toString() + "px";
+food.style.left = positionFood.x.toString() + "px";
 document.addEventListener("keydown", movement);
 
 function movement(e: KeyboardEvent) {
@@ -41,31 +57,60 @@ function movement(e: KeyboardEvent) {
 
 function changeDirection(action: string) {
 	if (action === "up") {
-		position.y -= 20;
-		snake.style.top = position.y.toString() + "px";
-		if (position.y <= 0) {
+		position.y -= fieldSize;
+		if (position.y <= 0 - fieldSize) {
 			alert("Das war ein Crash. GAME OVER");
+			restart();
+		} else {
+			snake.style.top = position.y.toString() + "px";
+			checkIfFood();
 		}
 	}
+
 	if (action === "down") {
-		position.y += 20;
-		snake.style.top = position.y.toString() + "px";
-		if (position.y >= 700) {
+		position.y += fieldSize;
+		if (position.y >= 700 - fieldSize) {
 			alert("Das war ein Crash. GAME OVER");
+			restart();
+		} else {
+			snake.style.top = position.y.toString() + "px";
+			checkIfFood();
 		}
 	}
 	if (action === "left") {
-		position.x -= 20;
-		snake.style.left = position.x.toString() + "px";
-		if (position.x <= 0) {
+		position.x -= fieldSize;
+		if (position.x <= 0 - fieldSize) {
 			alert("Das war ein Crash. GAME OVER");
+			restart();
+		} else {
+			snake.style.left = position.x.toString() + "px";
+			checkIfFood();
 		}
 	}
 	if (action === "right") {
-		position.x += 20;
-		snake.style.left = position.x.toString() + "px";
-		if (position.x >= 700) {
+		position.x += fieldSize;
+		if (position.x >= 700 - fieldSize) {
 			alert("Das war ein Crash. GAME OVER");
+			restart();
+		} else {
+			snake.style.left = position.x.toString() + "px";
+			checkIfFood();
 		}
+	}
+}
+function restart() {
+	position = {
+		x: 7 * fieldSize,
+		y: 7 * fieldSize,
+	};
+	snake.style.left = position.x.toString() + "px";
+	snake.style.top = position.y.toString() + "px";
+}
+
+function checkIfFood() {
+	if (position === positionFood) {
+		foodCount++;
+		food.style.top = positionFood.y.toString() + "px";
+		food.style.left = positionFood.x.toString() + "px";
 	}
 }
