8a90be9e6e54a56447d380112516b68dee129975
KRath87 <k.rath@mbd-team.de>
2024-09-05 11:47:39 +0200

    bewegbar

--- src/main.ts
+++ src/main.ts
@@ -7,3 +7,65 @@ for (let i = 0; i < 225; i++) {
 
 	playground.appendChild(div);
 }
+
+const position = {
+	x: 330,
+	y: 330,
+};
+
+let snakeSize = { width: 30, height: 30 };
+
+let snake = <HTMLDivElement>document.getElementById("snake");
+
+const food = <HTMLDivElement>document.getElementById("food");
+
+document.addEventListener("keydown", movement);
+
+function movement(e: KeyboardEvent) {
+	const code = e.code;
+	console.log(e.code);
+	if (code === "ArrowUp" || code === "KeyW") {
+		//up
+		changeDirection("up");
+	} else if (code === "ArrowDown" || code === "KeyS") {
+		//down
+		changeDirection("down");
+	} else if (code === "ArrowLeft" || code === "KeyA") {
+		//left
+		changeDirection("left");
+	} else if (code === "ArrowRight" || code === "KeyD") {
+		//right
+		changeDirection("right");
+	}
+}
+
+function changeDirection(action: string) {
+	if (action === "up") {
+		position.y -= 20;
+		snake.style.top = position.y.toString() + "px";
+		if (position.y <= 0) {
+			alert("Das war ein Crash. GAME OVER");
+		}
+	}
+	if (action === "down") {
+		position.y += 20;
+		snake.style.top = position.y.toString() + "px";
+		if (position.y >= 700) {
+			alert("Das war ein Crash. GAME OVER");
+		}
+	}
+	if (action === "left") {
+		position.x -= 20;
+		snake.style.left = position.x.toString() + "px";
+		if (position.x <= 0) {
+			alert("Das war ein Crash. GAME OVER");
+		}
+	}
+	if (action === "right") {
+		position.x += 20;
+		snake.style.left = position.x.toString() + "px";
+		if (position.x >= 700) {
+			alert("Das war ein Crash. GAME OVER");
+		}
+	}
+}
